dff3111f7c6ea982f90238e5094f9495ff6fbd03
pierrek6e <pierrecass75@gmail.com>
2022-04-02 17:12:55 +0700

    start handling errors

--- script.js
+++ script.js
@@ -2,54 +2,70 @@ const userName = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmedPassword = document.getElementById("confirm-password");
+const err = document.getElementById("error-message");
 
 function register() {
-  // checkEmail(email);
-  // verifyPassword(password, confirmedPassword);
-  checkName(userName);
+  if(verifyPassword(password, confirmedPassword) &&
+  checkEmail(email) &&
+  checkName(userName)) {
+      alert("Registration successful");
+  }
 }
 
 // Check name
 function checkName(userName) {
+  let nameVerification = false;
   const name = userName.value;
   if (name == null || name == undefined || name.length < 3) {
-    console.log("name is not ok");
-    //showError(name, "name is not valid");
+    showError(userName, "name is not valid");
   } else {
-    console.log("name is ok");
-    //showSuccess(email);
+    showSuccess(userName);
+    nameVerification = true;
   }
+  return nameVerification;
 }
 
 // Check email is valid
 function checkEmail(email) {
+  let emailVerification = false;
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(email.value.trim())) {
-    console.log("email ok");
-    //showSuccess(email);
+    showSuccess(email);
+    emailVerification = true;
   } else {
-    console.log("email not ok");
-    //showError(email, "Email is not valid");
+    showError(email, "Email is not valid");
   }
+  return emailVerification;
 }
 
 // Verify password
 const verifyPassword = (password, confirmedPassword) => {
-  let pwd = password.value;
-  let confPwd = confirmedPassword.value;
+  const pwd = password.value;
+  const confPwd = confirmedPassword.value;
+  let passwordVerification = false;
 
   if (pwd == null || pwd == undefined || pwd.length < 3) {
-    console.log("password not valid");
-    // showError(password, "password is not valid")
+    showError(password, "password is not valid");
   } else if (confPwd == null || confPwd == undefined || confPwd.length < 3) {
-    console.log("conf pwd not valid");
-    // showError(password, "password is not valid")
+    showError(password, "password is not valid");
   } else if (pwd !== confPwd) {
-    console.log("password do not match");
-    // showError(password, "Passwords do not match")
+    showError(password, "Passwords do not match");
   } else {
-    console.log("password is ok");
-    //showSuccess(password);
+    showSuccess(password);
+    showSuccess(confirmedPassword);
+    passwordVerification = true;
   }
+  return passwordVerification;
 };
+
+function showSuccess(inputField) {
+  inputField.classList.remove("error");
+  inputField.classList.add("success");
+}
+
+function showError(inputField, message) {
+  inputField.classList.remove("success");
+  inputField.classList.add("error");
+  err.innerText = message;
+}
