56717c91ecd08c192ed2732a989d162f53e6a28b
pierrek6e <pierrecass75@gmail.com>
2022-04-02 17:02:10 +0700

    Check Required fields

--- script.js
+++ script.js
@@ -5,19 +5,37 @@ const confirmedPassword = document.getElementById("confirm-password");
 const err = document.getElementById("error-message");
 
 function register() {
-  if(verifyPassword(password, confirmedPassword) &&
-  checkEmail(email) &&
-  checkName(userName)) {
-      alert("Registration successful");
+    
+  if (
+    !checkRequired([userName, email, password, confirmedPassword]) &&
+    checkEmail(email) &&
+    checkName(userName) &&
+    verifyPassword(password, confirmedPassword)
+  ) {
+    err.innerText = '';
+    alert("Registration successful");
   }
+}
+// Check required fields
+function checkRequired(arr) {
+  let isRequired = false;
+  for (let el of arr) {
+    if (el.value.trim() === "") {
+      showError(el, `${getFieldName(el)} is required`);
+      isRequired = true;
+    } else {
+        showSuccess(el);
+    }
+  }
+  return isRequired;
 }
 
 // Check name
 function checkName(userName) {
   let nameVerification = false;
   const name = userName.value;
-  if (name == null || name == undefined || name.length < 3) {
-    showError(userName, "name is not valid");
+  if (name.length < 3) {
+    showError(userName, "Name should be longer then 3 characters");
   } else {
     showSuccess(userName);
     nameVerification = true;
@@ -45,10 +63,10 @@ const verifyPassword = (password, confirmedPassword) => {
   const confPwd = confirmedPassword.value;
   let passwordVerification = false;
 
-  if (pwd == null || pwd == undefined || pwd.length < 3) {
-    showError(password, "password is not valid");
-  } else if (confPwd == null || confPwd == undefined || confPwd.length < 3) {
-    showError(password, "password is not valid");
+  if (pwd.length < 3) {
+    showError(password, "Password should be longer then 3 characters");
+  } else if (confPwd.length < 3) {
+    showError(password, "Password should be longer then 3 characters");
   } else if (pwd !== confPwd) {
     showError(password, "Passwords do not match");
   } else {
@@ -59,13 +77,20 @@ const verifyPassword = (password, confirmedPassword) => {
   return passwordVerification;
 };
 
+// Show validated fields
 function showSuccess(inputField) {
   inputField.classList.remove("error");
   inputField.classList.add("success");
 }
 
+// Show fields with errors
 function showError(inputField, message) {
   inputField.classList.remove("success");
   inputField.classList.add("error");
   err.innerText = message;
 }
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
